e9cf91d95c5b23af588f1916f0c101a523c3efa4
Nathan Almeida <nathan.dev2004@gmail.com>
2024-11-28 01:12:35 -0300

    fix: separando script do HTML e colocando em um arquivo separado em .js

--- js/login.js
+++ js/login.js
@@ -1,22 +1,22 @@
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-    try {
-      const response = await fetch('http://localhost:3000/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
-      });
+  try {
+    const response = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
 
-      if (response.ok) {
-        alert('Bem vindo!');
-        window.location.href = 'books.html';
-      } else {
-        throw new Error();
-      }
-    } catch {
-      alert('Usuário ou Senha incorretos, tente novamente');
+    if (response.ok) {
+      alert('Bem vindo!');
+      window.location.href = 'books.html';
+    } else {
+      throw new Error();
     }
+  } catch {
+    alert('Usuário ou Senha incorretos, tente novamente');
+  }
 });
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
-
-npm install cors
+| Version        | Dev                         | Project        |
+|----------------|-----------------------------|----------------|
+|1.0             |Nathan. A & GabeeDevs        |Back-End        |
